514761137770d278bde8ed8e09eef5e0488b47e0
S1M0N38 <bertolottosimone@gmail.com>
2025-12-04 18:43:44 +0100

    refactor(lua.utils): rename errors form BB_ERRORS to BB_ERROR_NAMES and BB_ERROR_CODES

--- balatrobot.lua
+++ balatrobot.lua
@@ -52,7 +52,7 @@ assert(SMODS.load_file("src/lua/core/dispatcher.lua"))() -- define BB_DISPATCHER
 
 -- Load gamestate and errors utilities
 BB_GAMESTATE = assert(SMODS.load_file("src/lua/utils/gamestate.lua"))()
-BB_ERRORS = assert(SMODS.load_file("src/lua/utils/errors.lua"))()
+assert(SMODS.load_file("src/lua/utils/errors.lua"))()
 
 -- Initialize Server
 local server_success = BB_SERVER.init()
--- src/lua/utils/errors.lua
+++ src/lua/utils/errors.lua
@@ -1,16 +1,36 @@
 -- src/lua/utils/errors.lua
--- Error Codes for BalatroBot API
+-- Error definitions for BalatroBot API
 
----@class ErrorCodes
----@field BAD_REQUEST string Client sent invalid data (protocol/parameter errors)
----@field INVALID_STATE string Action not allowed in current game state
----@field NOT_ALLOWED string Game rules prevent this action
----@field INTERNAL_ERROR string Server-side failure (runtime/execution errors)
+---@alias ErrorName
+---| "BAD_REQUEST" Client sent invalid data (protocol/parameter errors)
+---| "INVALID_STATE" Action not allowed in current game state
+---| "NOT_ALLOWED" Game rules prevent this action
+---| "INTERNAL_ERROR" Server-side failure (runtime/execution errors)
 
----@type ErrorCodes
-return {
+---@alias ErrorNames table<ErrorName, ErrorName>
+
+---@alias ErrorCode
+---| -32000 # INTERNAL_ERROR
+---| -32001 # BAD_REQUEST
+---| -32002 # INVALID_STATE
+---| -32003 # NOT_ALLOWED
+
+---@alias ErrorCodes table<ErrorName, ErrorCode>
+
+---@type ErrorNames
+BB_ERROR_NAMES = {
+  INTERNAL_ERROR = "INTERNAL_ERROR",
   BAD_REQUEST = "BAD_REQUEST",
   INVALID_STATE = "INVALID_STATE",
   NOT_ALLOWED = "NOT_ALLOWED",
-  INTERNAL_ERROR = "INTERNAL_ERROR",
 }
+
+---@type ErrorCodes
+BB_ERROR_CODES = {
+  INTERNAL_ERROR = -32000,
+  BAD_REQUEST = -32001,
+  INVALID_STATE = -32002,
+  NOT_ALLOWED = -32003,
+}
+
+return BB_ERROR_NAMES, BB_ERROR_CODES
