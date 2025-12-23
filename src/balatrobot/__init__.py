0fa8bf74b8c9b929191f2ddce4a3724d5b4152c7
S1M0N38 <bertolottosimone@gmail.com>
2025-12-23 16:31:38 +0100

    feat!: update to v1.0.0

--- src/balatrobot/__init__.py
+++ src/balatrobot/__init__.py
@@ -3,5 +3,5 @@
 from balatrobot.config import Config
 from balatrobot.manager import BalatroInstance
 
-__version__ = "0.7.5"
+__version__ = "1.0.0"
 __all__ = ["BalatroInstance", "Config", "__version__"]
